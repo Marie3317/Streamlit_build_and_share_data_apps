32171d5c97719450f4ff69a77bdcbe0e78fe1418
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-08 18:19:52 +0200

    my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -6,13 +6,13 @@ link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_car = pd.read_csv(link)
 st.write(df_car)
 
-st.line_chart(df_car['MAX_TEMPERATURE_C'])
+#st.line_chart(df_car['MAX_TEMPERATURE_C'])
 
-viz_correlation = sns.heatmap(df_car.corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
+#viz_correlation = sns.heatmap(df_car.corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
 
-st.pyplot(viz_correlation.figure)
+#st.pyplot(viz_correlation.figure)
 
-st.title('Hello Wilders, welcome to my application!')
+#st.title('Hello Wilders, welcome to my application!')
 
 name = st.text_input("Please give me your name :")
 name_length = len(name)

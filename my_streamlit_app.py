ddbbb14ed660299cd9bb981f378f2b94de7d1bd5
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-08 18:17:04 +0200

    my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,11 +1,8 @@
-import streamlit as st
-import pandas as pd
-
 st.title('Hello Wilders, welcome to my application!')
 
-st.write("I enjoy to discover stremalit possibilities")
+st.write("I enjoy to discover streamlit possibilities")
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_weather = pd.read_csv(link)
 st.write(df_weather)
 

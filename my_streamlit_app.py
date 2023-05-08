0824652dab000d1839d9cb62422cb6c273f10162
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-08 18:32:04 +0200

    my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -7,22 +7,30 @@ st.set_page_config(
     page_title="Quête WildCodeSchool",
     layout="wide",
     page_icon="📐")
-
+# titre
 st.title('Hello Wilders, welcome to my application!')
 
+#Sous_titre
 st.write("I enjoy to discover streamlit possibilities")
 
+#Sous_titre2
 st.write("Voici le DF avec lequel je vais travailler.")
 
+#Téléchargement du DF
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_car = pd.read_csv(link)
+
+#Affichage du DF
 st.write(df_car)
 
+#Création liste pour pays
+liste_pays = = list(df_car["continent"])
 st.line_chart(df_car['mpg'])
+selection = st.selectbox("Pays : ", liste_pays)
 
 viz_correlation = sns.heatmap(df_car.corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
 
-#st.pyplot(viz_correlation.figure)
+st.pyplot(viz_correlation.figure)
 
 #st.title('Hello Wilders, welcome to my application!')
 

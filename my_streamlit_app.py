d095c40fd0ed198b1150270eb30f75c17fdb0432
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-08 18:18:18 +0200

    my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,16 +3,12 @@ st.title('Hello Wilders, welcome to my application!')
 st.write("I enjoy to discover streamlit possibilities")
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
-df_weather = pd.read_csv(link)
-st.write(df_weather)
+df_car = pd.read_csv(link)
+st.write(df_car)
 
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+st.line_chart(df_car['MAX_TEMPERATURE_C'])
 
-import seaborn as sns
-viz_correlation = sns.heatmap(df_weather.corr(), 
-								center=0,
-								cmap = sns.color_palette("vlag", as_cmap=True)
-								)
+viz_correlation = sns.heatmap(df_car.corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
 
 st.pyplot(viz_correlation.figure)
 

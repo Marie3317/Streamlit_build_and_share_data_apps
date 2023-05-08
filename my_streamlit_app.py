8639ca8ac020c176524db901e97bad173dcf563b
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-08 18:21:10 +0200

    my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -16,6 +16,6 @@ st.write(df_car)
 
 #st.title('Hello Wilders, welcome to my application!')
 
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
+#name = st.text_input("Please give me your name :")
+#name_length = len(name)
+#st.write("Your name has ",name_length,"characters")

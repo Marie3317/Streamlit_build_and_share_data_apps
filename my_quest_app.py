532163c854a3626080242d488bde55ac2d39bda2
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-09 19:55:29 +0200

    my_quest_app.py

--- my_quest_app.py
+++ my_quest_app.py
@@ -38,24 +38,14 @@ df_car= df_car.query("continent == @pays")
 
 st.dataframe(df_car)
 
-#top kpi's
+# top mpg
 total_mpg = int(df_car["mpg"].sum())
 average_mpg = round(df_car["mpg"].mean(),2)
 
-left_column, right_column = st.columns(2)
-with left_column :
-    st.subheader("Somme des mpg :")
-    st.subheader(f"Somme {total_mpg}")
-with right_column :
-    st.subheader("Moyenne des mpg :")
-    st.subheader(f"Moyenne {average_mpg}")
-
-    st.markdown("---")
-
 # Map corrélation
-#viz_correlation = sns.heatmap(df_weather.corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
-#viz_correlation.update_layout(figsize =(15,15))
-#st.pyplot(viz_correlation.figure)
+viz_correlation = sns.heatmap(df_weather.corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True))
+viz_correlation.update_layout(figsize =(15,15))
+st.pyplot(viz_correlation.figure)
 
 # Bar chart
 #fig_XX = px.bar(df_car, x = "mpg",

cc8f89dfa70aa3d447609fdd16c533745fab7d60
Marie3317 <131977082+Marie3317@users.noreply.github.com>
2023-05-10 11:27:20 +0200

    my_quest_app.py

--- my_quest_app.py
+++ my_quest_app.py
@@ -80,7 +80,7 @@ def main():
 	# Corrélation entre weightlbs et hp
 	fig5, ax = plt.subplots()
 	plt.figure(figsize=(10, 4))
-	sns.histplot(x = "year", data = df_select_pays)
+	sns.barplot(df_select_pays, x="year", y="hp")
 	st.pyplot(fig5)
 	st.write("On s'apperçoit que plus le hp (horse power) est important plus les cylinders (puissance) augmentent.")
 	
